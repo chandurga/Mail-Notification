c78c76f3c6963e4f7f992eb55a04a0cae56c0e88
chandurga <durgaprasadmajji33@gmail.com>
2024-06-14 14:52:10 +0530

    added new test

---
+++ src/test/java/google/LogInTest.java
@@ -0,0 +1,14 @@
+package google;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LogInTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.close();
+    }
+}
