90cddcdf2dd23a368f8b6f25938f26ef0476d729
chandurga <durgaprasadmajji33@gmail.com>
2024-06-14 15:00:09 +0530

    Added the test annotation

--- src/test/java/google/LogInTest.java
+++ src/test/java/google/LogInTest.java
@@ -1,10 +1,12 @@
 package google;
 
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LogInTest {
-    public static void main(String[] args) {
+    @Test
+    public void assertThatUsrCanOpenBrowser() {
         WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
         System.out.println(driver.getTitle());
